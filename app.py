655569a891f48a287711bc4fbc55b1b713953352
Orpheus Peng <stannolite@gmail.com>
2020-11-13 23:40:34 -0800

    Set up custom JSON encoder

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
 from database.db import initialize_db
+from flask import Flask
 from secrets import FLASK_APP_SECRET_KEY
+from utils.JsonEncoder import MongoEngineJsonEncoder
 
 app = Flask(__name__)
 app.secret_key = FLASK_APP_SECRET_KEY
+app.json_encode = MongoEngineJsonEncoder
 
 initialize_db(app)
 
