fd9a519346cb0fcca4b9fd164836ff88565573bd
Orpheus Peng <stannolite@gmail.com>
2020-11-13 21:45:06 -0800

    Add database setup file

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from database.db import initialize_db
+from secrets import FLASK_APP_SECRET_KEY
 
 app = Flask(__name__)
+app.secret_key = FLASK_APP_SECRET_KEY
+
+initialize_db(app)
 
 if __name__ == '__main__':
     app.run()
