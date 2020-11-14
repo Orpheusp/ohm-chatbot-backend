02de0346e37bdcc493f5ba64db3d42923c18c850
Orpheus Peng <stannolite@gmail.com>
2020-11-13 23:43:17 -0800

    Install flask-restful dependency

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from database.db import initialize_db
 from flask import Flask
+from flask_restful import Api
 from secrets import FLASK_APP_SECRET_KEY
 from utils.JsonEncoder import MongoEngineJsonEncoder
 
 app = Flask(__name__)
 app.secret_key = FLASK_APP_SECRET_KEY
-app.json_encode = MongoEngineJsonEncoder
 
 initialize_db(app)
+app.json_encoder = MongoEngineJsonEncoder
+api = Api(app)
 
 if __name__ == '__main__':
     app.run()
