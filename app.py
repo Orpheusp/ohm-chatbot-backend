2ac244dc80b4bf47f9b95b0c57c59ab949daea24
Orpheus Peng <stannolite@gmail.com>
2020-11-15 11:45:46 -0800

    Add TutorialCards as a resource to the app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from secrets import FLASK_APP_SECRET_KEY
 from utils.JsonEncoder import MongoEngineJsonEncoder
 from resources.ChatCards import ChatCards
+from resources.TutorialCards import TutorialCards
 
 app = Flask(__name__)
 app.secret_key = FLASK_APP_SECRET_KEY
@@ -13,7 +14,9 @@ app.json_encoder = MongoEngineJsonEncoder
 api = Api(app)
 
 # Resources for testing purposes
-api.add_resource(ChatCards, '/chatcards', '/chatcards/<string:card_id>')
+api.add_resource(ChatCards, '/chat-cards', '/chat-cards/<string:card_id>')
+api.add_resource(
+    TutorialCards, '/tutorial-cards', '/tutorial-cards/<string:card_id>')
 
 if __name__ == '__main__':
     app.run()
