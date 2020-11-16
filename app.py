3a8247c2b7a0f063b645fbf80fb96bae40bfee33
Orpheus Peng <stannolite@gmail.com>
2020-11-15 16:49:58 -0800

    Set up the Flask Sessions environment

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from database.db import initialize_dbs
+from database.db import initialize_db, initialize_sessions
 from flask import Flask
 from flask_restful import Api
-from secrets import FLASK_APP_SECRET_KEY
 from utils.JsonEncoder import MongoEngineJsonEncoder
 from resources.ChatCards import ChatCards
 from resources.TutorialCards import TutorialCards
 from resources.InformationCards import InformationCards
 
 app = Flask(__name__)
-app.secret_key = FLASK_APP_SECRET_KEY
 
-initialize_dbs()
+initialize_db()
+initialize_sessions(app)
 app.json_encoder = MongoEngineJsonEncoder
 api = Api(app)
 
--- database/db.py
+++ database/db.py
@@ -1,30 +1,45 @@
 from mongoengine import connect
 from pymongo import MongoClient
 from typing import Optional
-from secrets import DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_CARDS_HOSTNAME, DATABASE_SESSIONS_HOSTNAME
+from secrets import DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOSTNAME
+from flask_session import Session
+from secrets import FLASK_APP_SECRET_KEY
 
 _cards_db: Optional[MongoClient] = None
 _sessions_db: Optional[MongoClient] = None
+_sessions: Optional[Session] = None
 
 
-def initialize_dbs():
+def initialize_db():
     global _cards_db
-    global _sessions_db
 
     _cards_db = connect(
         db='Cards',
         alias='cards',
         username=DATABASE_USERNAME,
         password=DATABASE_PASSWORD,
-        host=DATABASE_CARDS_HOSTNAME
+        host=DATABASE_HOSTNAME
     )
+
+
+def initialize_sessions(app):
+    global _sessions_db
+
     _sessions_db = connect(
         db='Sessions',
         alias='sessions',
         username=DATABASE_USERNAME,
         password=DATABASE_PASSWORD,
-        host=DATABASE_SESSIONS_HOSTNAME
+        host=DATABASE_HOSTNAME
     )
+
+    app.config['SECRET_KEY'] = FLASK_APP_SECRET_KEY
+    app.config['SESSION_TYPE'] = 'mongodb'
+    app.config['SESSION_USE_SIGNER'] = True
+    app.config['SESSION_MONGODB'] = _sessions_db
+    app.config['SESSION_MONGODB_DB'] = 'Sessions'
+    app.config['SESSION_MONGODB_COLLECT'] = 'sessions'
+    _sessions = Session(app)
 
 
 def fetch_cards_db():
